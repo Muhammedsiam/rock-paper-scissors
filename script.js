286491bc1b943f99a54af3896b537377e3c7fffa
muhammed_siam <161434907+Muhammedsiam@users.noreply.github.com>
2026-04-26 22:34:43 +0300

    Add playGame function with 5 rounds and final winner logic

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function getComputerChoice() {
-    const randomNumber = Math.random();
-    if (randomNumber < 0.33) {
-        return 'rock';
-    } else if (randomNumber < 0.66) {
-        return 'paper';
+    function getComputerChoice() {
+        const randomNumber = Math.random();
+        if (randomNumber < 0.33) {
+            return 'rock';
+        } else if (randomNumber < 0.66) {
+            return 'paper';
+        } else {
+            return 'scissors';
+        }
+    }
+
+    function getHumanChoice() {
+        let choice = prompt('Enter your choice: ');
+        choice = choice.toLowerCase();
+        return choice;
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log('Draw.');
+        } else if (
+            (humanChoice === 'rock' && computerChoice === 'scissors') ||
+            (humanChoice === 'paper' && computerChoice === 'rock') ||
+            (humanChoice === 'scissors' && computerChoice === 'paper')
+        ) {
+            humanScore++;
+            console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
+        } else {
+            computerScore++;
+            console.log(`You Lose, lol. ${computerChoice} beats ${humanChoice}`);
+        }
+    }
+
+    for (let round = 1; round <= 5; round++) {
+        console.log(`Round ${round}`);
+        playRound(getHumanChoice(), getComputerChoice())
+    }
+
+    if (computerScore > humanScore) {
+        console.log('Computer Won, lol.')
+    } else if (computerScore === humanScore) {
+        console.log('Tie, what a waste');
     } else {
-        return 'scissors';
+        console.log('You Won!')
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt('Enter your choice: ');
-    choice = choice.toLowerCase();
-    return choice;
-}
+playGame()
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        console.log('Draw.');
-    } else if (
-        (humanChoice === 'rock' && computerChoice === 'scissors') ||
-        (humanChoice === 'paper' && computerChoice === 'rock') ||
-        (humanChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-        humanScore++;
-        console.log(`You Win, ${humanChoice} beats ${computerChoice}`);
-    } else {
-        computerScore++;
-        console.log(`You Lose, lol. ${computerChoice} beats ${humanChoice}`);
-    }
-}
-playRound(getHumanChoice(), getComputerChoice())

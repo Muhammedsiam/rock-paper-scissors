52811c313873db18e9b0947f079c20a5dd41a057
muhammed_siam <161434907+Muhammedsiam@users.noreply.github.com>
2026-04-26 20:41:36 +0300

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,10 @@
+function getComputerChoice() {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        return 'rock';
+    } else if (randomNumber < 0.66) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
